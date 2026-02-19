9df8ecc2f877669aa354f8022eba14925f671718
unknown <omsatapathy7@gmail.com>
2026-02-19 17:15:15 +0530

    marked headless as true for CI/CD

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,7 +36,10 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'], headless: false },
+        use: { 
+          ...devices['Desktop Chrome'], 
+          headless: !!process.env.CI ? true : false 
+        },
     },
 
     // {
