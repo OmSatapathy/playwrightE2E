e2de37f3a90ffe4f684974f93717ac3a8a624953
Om Satapathy <omsatapathy7@gmail.com>
2026-02-27 15:35:02 +0530

    data driven test

--- tests/ui/readExceltest.spec.ts
+++ tests/ui/readExceltest.spec.ts
@@ -3,13 +3,13 @@ import * as XLSX from 'xlsx';
 
 test('Login using Excel data', async ({ page }) => {
 
+  
     await page.goto("https://freelance-learn-automation.vercel.app/login");
     await page.waitForLoadState('domcontentloaded')
 
     const workbook = XLSX.readFile('testdata/manualdata.xlsx');
     const sheet = workbook.Sheets['Sheet1'];
-    const data = XLSX.utils.sheet_to_json(sheet);
-
+    const data = XLSX.utils.sheet_to_json(sheet) ;
     for (const user of data as any[]) {
 
         await page.getByPlaceholder("Enter Email").fill(user.username)
